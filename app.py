6b3e99634dbd8ecf154ef1af81e5cd010cdc7cb3
UdithaChalla08 <udithachalla2005@gmail.com>
2025-07-03 11:59:45 +0000

    Updated model and login page UI

--- app.py
+++ app.py
@@ -93,6 +93,5 @@ def predict_form():
     return render_template("index.html", prediction=prediction, message=message)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, port=1234)
+    port=int(os.environ.get("PORT",10000))
+    app.run(host='0.0.0.0',port=port)
